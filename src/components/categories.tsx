6fd8b34fcd4975ca010ea27cb5b3ad9b963e3a9c
Mithu <142711288+Mithu3311@users.noreply.github.com>
2024-12-30 13:42:42 +0500

    added Category and Promotion Components

---
+++ src/components/categories.tsx
@@ -0,0 +1,87 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div className="mb-[100px] mt-[100px]">
+      {/* Heading */}
+      <div className="text-center mb-10">
+        <h1 className="text-myBlackHead scroll-m-20 text-xl font-extrabold tracking-tight lg:text-2xl">
+          Our Categories
+        </h1>
+        <div className="flex mt-2 justify-center">
+          <div className="w-16 h-1 rounded-full bg-myOrange inline-flex" />
+        </div>
+      </div>
+      {/* Categories Starts from Here */}
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 md:grid-cols-2">
+        {/* Category 1 */}
+        <div className="mb-7 lg:mb-0 overflow-hidden z-10 mx-auto group relative">
+            <Link href={'#'}>
+          <Image
+            src={"/pictures/Category-1.avif"}
+            alt="Stationary"
+            width={350}
+            height={350}
+            className="rounded-xl duration-500 group-hover:scale-125"
+          />
+          <div className="absolute -bottom-24 group-hover:bottom-0 duration-500 scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-3xl bg-myBlackHead/70 text-center text-myWhite w-full">
+            <h1 className="uppercase">Stationary</h1>
+          </div>
+          </Link>
+        </div>
+
+        {/* Category 2 */}
+        <div className="mb-7 lg:mb-0 overflow-hidden z-10 mx-auto group relative">
+            <Link href={'#'}>
+          <Image
+            src={"/pictures/Category-2.webp"}
+            alt="Sports"
+            width={350}
+            height={350}
+            className="rounded-xl duration-500 group-hover:scale-125"
+          />
+          <div className="absolute -bottom-24 group-hover:bottom-0 duration-500 scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-3xl bg-myBlackHead/70 text-center text-myWhite w-full">
+            <h1 className="uppercase">Sports</h1>
+          </div>
+          </Link>
+        </div>
+
+        {/* Category 3 */}
+        <div className="mb-7 lg:mb-0 overflow-hidden z-10 mx-auto group relative">
+            <Link href={'#'}>
+          <Image
+            src={"/pictures/Category-3.jpg"}
+            alt="Sports"
+            width={350}
+            height={350}
+            className="rounded-xl duration-500 group-hover:scale-125"
+          />
+          <div className="absolute -bottom-24 group-hover:bottom-0 duration-500 scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-3xl bg-myBlackHead/70 text-center text-myWhite w-full">
+            <h1 className="uppercase">Electronics</h1>
+          </div>
+          </Link>
+        </div>
+
+        {/* Category 4 */}
+        <div className="mb-7 lg:mb-0 overflow-hidden z-10 mx-auto group relative">
+            <Link href={'#'}>
+          <Image
+            src={"/pictures/Category-4.png"}
+            alt="Sports"
+            width={350}
+            height={350}
+            className="rounded-xl duration-500 group-hover:scale-125"
+          />
+          <div className="absolute -bottom-24 group-hover:bottom-0 duration-500 scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-3xl bg-myBlackHead/70 text-center text-myWhite w-full">
+            <h1 className="uppercase">Photo Studio</h1>
+          </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
--- src/components/promotion.tsx
+++ src/components/promotion.tsx
@@ -13,25 +13,38 @@ const Promotion = () => {
         </div>
       </div>
 
-      {/* Promotions Start Here */}
+      {/* Promotions Start Here */} 
 
-      <div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
         {/* 1st Child */}
         <div
-          className="hero h-[25rem] promo-1"
+          className="hero h-[25rem] promo-1 rounded-xl"
         >
-          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-overlay bg-opacity-70 rounded-xl"></div>
           <div className="hero-content text-neutral-content text-center">
             <div className="max-w-md">
-              <h1 className="scroll-m-20 text-4xl font-bold tracking-tight uppercase">Get upto 60% off</h1>
-              <p className="mb-5">
-                Flash Sale, get upto 60% off on the Variant 
+              <h1 className="scroll-m-20 text-4xl font-bold tracking-tight uppercase text-myWhite">Get upto <span className="text-myOrange">60%</span> off</h1>
+              <p className="scroll-m-20 text-base font-medium tracking-tight text-myWhite mt-2">
+                Flash Sale, get upto 60% off on the Stationary Items. 
               </p>
             </div>
           </div>
         </div>
 
         {/* 2nd Child */}
+        <div
+          className="hero h-[25rem] promo-2 rounded-xl"
+        >
+          <div className="hero-overlay bg-opacity-70 rounded-xl"></div>
+          <div className="hero-content text-neutral-content text-center">
+            <div className="max-w-md">
+              <h1 className="scroll-m-20 text-4xl font-bold tracking-tight uppercase text-myWhite">Get upto <span className="text-myOrange">30%</span> off</h1>
+              <p className="scroll-m-20 text-base font-medium tracking-tight text-myWhite mt-2">
+                Flash Sale, get upto 30% off on the New Sports Items.  
+              </p>
+            </div>
+          </div>
+        </div>
 
       </div>
     </div>
